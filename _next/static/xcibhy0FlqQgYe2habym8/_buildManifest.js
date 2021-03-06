f707f3a8b602e37f43e25e52f83c00cb865078d6
leroy0211 <leroy0211@users.noreply.github.com>
2021-03-06 12:04:49 +0000

    deploy: 2f62933757a71dcc35e8673640b86cd3d0aea9df

--- _next/static/xcibhy0FlqQgYe2habym8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-6847aee810f4c00a86ea.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4ab65733f60df4d9fc42.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
